f367ed960c4b25c5b7744eb25948475ec4a2d21f
emilystap <eas.stapleton@gmail.com>
2024-02-07 14:06:58 -0500

    start viewmodel

---
+++ app/src/main/java/com/example/smartvest/ui/states/SettingsUiState.kt
@@ -0,0 +1,9 @@
+package com.example.smartvest.ui.states
+
+data class SettingsUiState(
+    val locationEnabled: Boolean = false,
+    val smsEnabled: Boolean = false,
+    val storedSmsNumber: String = ""
+) {
+
+}
---
+++ app/src/main/java/com/example/smartvest/ui/viewmodels/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.smartvest.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+
+}
